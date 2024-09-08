913001bfa984779c4a0b75d447c427f04fe5529a
Цвиркунов Михаил <tsvirkunov@profilum.ru>
2024-09-08 09:47:28 +0600

    added_plots

--- presentation_generation_service/src/getters/get_text.py
+++ presentation_generation_service/src/getters/get_text.py
@@ -2,6 +2,6 @@ from commutators.gpt_service_comutator import get_gpt_comutator
 
 
 def get_shorted_text_by_gpt(text: str):
-    prompt = f"Сократи данный текст: {text}"
+    prompt = f"Очень сильно сократи данный текст: {text}"
     res = get_gpt_comutator(prompt, 10)
     return res
